fc734ed8a20be9bb983ca8dd6961c2d48a1e2a98
kalpanag04 <kalpanag808@gmail.com>
2022-06-15 13:20:24 +0530

    EmpWage Commit Added

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
     public const int NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
     static void Main(string[] args)
     {
         int empHrs = 0;
         int empWage = 0;
-        int totalEmpWage = 0;
-        for(int day=0; day<NUM_OF_WORKING_DAYS; day++)
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
@@ -29,10 +31,10 @@
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage : "+empWage);
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Days:"+ totalWorkingDays+" Emp Hrs :"+empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Emp Wage : "+totalEmpWage);
     }
 }
