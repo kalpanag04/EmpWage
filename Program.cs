30099d35ec1fd10d37b31aad0d9b32488244aac0
kalpanag04 <kalpanag808@gmail.com>
2022-06-15 12:46:07 +0530

    Added: EmpWage Calculated

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        int IS_FULL_TIME= 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
         int empHrs = 0;
@@ -10,16 +11,18 @@
 
         Random random = new Random();
 
-        int empCheck = random.Next(0, 2);
+        int empCheck = random.Next(0, 3);
         Console.WriteLine(empCheck);
         if (empCheck == IS_FULL_TIME)
         {
-            Console.WriteLine("Employee is present");
             empHrs = 8;
+        }
+        else if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
         }
         else
         {
-            Console.WriteLine("Employee is Absent");
             empHrs = 0;
         }
         empWage = empHrs*EMP_RATE_PER_HOUR;
