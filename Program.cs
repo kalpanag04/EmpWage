a4118b191071ad7ffb12ab232dd4a2258fd6e626
kalpanag04 <kalpanag808@gmail.com>
2022-06-15 12:13:43 +0530

    Added Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class EmpWage
+{
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME= 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        Console.WriteLine(empCheck);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+    }
+}
