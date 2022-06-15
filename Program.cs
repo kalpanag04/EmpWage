13c24f028752101593ac8d99331d83d50e2cc95d
kalpanag04 <kalpanag808@gmail.com>
2022-06-15 12:56:01 +0530

    Switch Statement

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿class Employee
 {
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
     static void Main(string[] args)
     {
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-
         int empHrs = 0;
         int empWage = 0;
 
@@ -13,17 +12,19 @@
 
         int empCheck = random.Next(0, 3);
         Console.WriteLine(empCheck);
-        if (empCheck == IS_FULL_TIME)
+        switch(empCheck)
         {
-            empHrs = 8;
-        }
-        else if (empCheck == IS_PART_TIME)
-        {
-            empHrs = 4;
-        }
-        else
-        {
-            empHrs = 0;
+            case IS_FULL_TIME :
+                empHrs = 8;
+                break;
+
+            case IS_PART_TIME :
+                empHrs = 4;
+                break;
+
+            default:
+                empHrs = 0;
+                break;
         }
         empWage = empHrs*EMP_RATE_PER_HOUR;
         Console.WriteLine(empWage);
