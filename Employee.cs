27240c58ca63fb7f834dbe20e0fa30e82684030f
kalpanag04 <kalpanag808@gmail.com>
2022-07-24 13:07:36 +0530

    Check Employee is Present or Absent

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+        public abstract class AttendenceCheck
+        {
+            public abstract void Attendence();
+        }
+        class Employee : AttendenceCheck
+        {
+            public override void Attendence()
+            {
+                //random number generation
+                Random rand = new Random();
+                int isPresent = rand.Next(2);
+
+                if (isPresent == 1)
+                    Console.WriteLine("Employee is Present.");
+                if (isPresent == 0)
+                    Console.WriteLine("Employee is Absent");
+            }
+        }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,40 +1,15 @@
-﻿class Employee
+﻿using System;
+
+namespace EmpWage
 {
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 2;
-    public const int MAX_HRS_IN_MONTH = 10;
-    static void Main(string[] args)
+    class Program
     {
-        int empHrs = 0;
-        int empWage = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+        static void Main(string[] args)
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            Console.WriteLine(empCheck);
-
-            switch (empCheck)
-            {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Days:"+ totalWorkingDays+" Emp Hrs :"+empHrs);
+            Console.WriteLine("Welcome to Employee wage Calculation ");
+            //check employee attendence
+            Employee employee = new Employee();
+            employee.Attendence();
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Total Emp Wage : "+totalEmpWage);
     }
 }
