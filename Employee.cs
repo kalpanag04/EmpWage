ea79dd56b67091ecfb88e9cd1d82d9fa85ffdde0
kalpanag04 <kalpanag808@gmail.com>
2022-07-24 13:58:08 +0530

    Calculate MonthlyWage

--- Employee.cs
+++ Employee.cs
@@ -8,42 +8,55 @@ namespace EmpWage
 {
     class Employee
     {
+
         public void AttendenceCheck()
         {
             //constants
             const int WAGE_PER_HR = 20;
+            const int WORKING_DAYS = 20;
             const int IS_FULL_TIME = 8;
             const int IS_PART_TIME = 4;
 
             //variables
             int dailyWage = 0;
-
+            int days = 0;
+            int isPresent;
+            int totalWage = 0;
             //random number generation
             Random rand = new Random();
-            int isPresent = rand.Next(0, 3);
-
-            if (isPresent == 1)
+            //calculating for month
+            for (days = 1; days <= WORKING_DAYS; days++)
             {
-                dailyWage = WAGE_PER_HR * IS_FULL_TIME;
-                Console.WriteLine("Employee is present Fulltime");
-                Console.WriteLine($"Daily wage is {dailyWage}");
+                isPresent = rand.Next(0, 3);
 
+                //using switch case
+                switch (isPresent)
+                {
+                    case 1:
+                        {
+                            dailyWage = WAGE_PER_HR * IS_FULL_TIME;
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            dailyWage = WAGE_PER_HR * IS_PART_TIME;
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                }
+                totalWage += dailyWage;
             }
-            else if (isPresent == 2)
-            {
-                dailyWage = WAGE_PER_HR * IS_PART_TIME;
-                Console.WriteLine("Employee is present Parttime");
-                Console.WriteLine($"Daily wage is {dailyWage}");
+            Console.WriteLine($"Montly wage is {totalWage}");
 
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                Console.WriteLine($"Daily wage is {dailyWage}");
-            }
+
 
         }
 
     }
-
 }
