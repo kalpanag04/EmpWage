ed23114fe072c4d91b111b423aca8df1439e0321
kalpanag04 <kalpanag808@gmail.com>
2022-07-24 13:18:41 +0530

    Employee DailyWage

--- Employee.cs
+++ Employee.cs
@@ -6,23 +6,42 @@ using System.Threading.Tasks;
 
 namespace EmpWage
 {
-        public abstract class AttendenceCheck
-        {
-            public abstract void Attendence();
-        }
-        class Employee : AttendenceCheck
-        {
-            public override void Attendence()
-            {
-                //random number generation
-                Random rand = new Random();
-                int isPresent = rand.Next(2);
+    public abstract class AttendenceCheck
+    {
+        public abstract void Attendence();
+        public abstract void DailyWage(int attendence);
 
-                if (isPresent == 1)
-                    Console.WriteLine("Employee is Present.");
-                if (isPresent == 0)
-                    Console.WriteLine("Employee is Absent");
+    }
+    class Employee : AttendenceCheck
+    {
+        public override void Attendence()
+        {
+            //random number generation
+            Random rand = new Random();
+            int isPresent = rand.Next(2);
+
+            DailyWage(isPresent);
+        }
+
+        public override void DailyWage(int isPresent)
+        {
+            //constants
+            const int WAGE_PER_HR = 20;
+            const int IS_FULL_TIME_HOURS = 8;
+            const int FULL_TIME = 1;
+            const int IS_ABSENT = 0;
+            //variables
+            int dailyWage = 0;
+            if (isPresent == FULL_TIME)
+            {
+                dailyWage = WAGE_PER_HR * IS_FULL_TIME_HOURS;
+                Console.WriteLine($"Daily wage is {dailyWage}");
+            }
+            if (isPresent == IS_ABSENT)
+            {
+                Console.WriteLine($"Daily wage is {dailyWage}");
             }
         }
-    
+
+    }
 }
